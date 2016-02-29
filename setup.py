652041370011711d9ff24d0e8c494d487460041a
Alex Conway <alex.c@voxy.com>
2016-02-28 20:32:36 -0500

    Bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bluecanary',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
